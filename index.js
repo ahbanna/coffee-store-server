74e3a839779789d8099b361fe73e4e65744b97cb
Al Hasanul Banna <tutul5800@gmail.com>
2023-05-14 23:16:34 +0600

    update integration

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -27,6 +27,74 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // come from node mongodb crud start
+    const coffeeCollection = client.db("coffeeDB").collection("coffee");
+    // come from node mongodb crud end
+
+    // CREATE api to receive data from client side starts
+    app.post("/coffee", async (req, res) => {
+      const newCOffee = req.body;
+      console.log(newCOffee);
+      // insert or add data to the mongodb database
+      const result = await coffeeCollection.insertOne(newCOffee);
+      // send result to the client
+      res.send(result);
+    });
+    // CREATE api to receive data from client side end
+
+    // READ starts
+    app.get("/coffee", async (req, res) => {
+      const cursor = coffeeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    // READ end
+
+    // DELETE starts
+    app.delete("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeeCollection.deleteOne(query);
+      res.send(result);
+    });
+    // DELETE end
+
+    // UPDATE starts
+    app.get("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeeCollection.findOne(query);
+      res.send(result);
+    });
+    // To get current information from client side
+    app.put("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      //after getting data from client side, now have to send data to mongodb
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const coffee = req.body;
+      const updatedcoffee = {
+        $set: {
+          name: coffee.name,
+          quantity: coffee.quantity,
+          supplier: coffee.supplier,
+          taste: coffee.taste,
+          category: coffee.category,
+          details: coffee.details,
+          photo: coffee.photo,
+        },
+      };
+      const result = await coffeeCollection.updateOne(
+        filter,
+        updatedcoffee,
+        options
+      );
+      res.send(result);
+    });
+    // UPDATE end
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
